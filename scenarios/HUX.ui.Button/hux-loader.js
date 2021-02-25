b56e80461a7c3c6dd4ad3101908a8e00f63ac3d9
Marc Jentsch <marc.jentsch@hsh-berlin.com>
2021-02-25 15:25:24 +0100

    juji

--- scenarios/HUX.ui.Button/hux-loader.js
+++ scenarios/HUX.ui.Button/hux-loader.js
@@ -1,3 +1,28 @@
+function loadCssStyle(path) {
+  let link = document.createElement("link");
+  link.setAttribute("type", "text/css");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("href", path);
+  document.head.appendChild(link);
+}
+
+
+function loadLessStyle(path) {
+  //TODO
+}
+
+function loadScript(href, defer,  onLoad){
+  let script = document.createElement("script");
+  if(onLoad){
+    script.onload = onLoad
+  }
+  script.setAttribute("src", href);
+  if(defer){
+    script.setAttribute("defer", '');
+  }
+  document.head.appendChild(script);
+}
+
 // get hux-version
 let huxVersion = document.documentElement.getAttribute("data-hux-version") || 6;
 
@@ -6,17 +31,16 @@ let path = location.pathname.replace("/scenarios/", "");
 path = path.substring(0, path.indexOf("/"));
 document.head.getElementsByTagName("title")[0].innerHTML = path;
 
-// add jQuery
-let jqScript = document.createElement("script");
-jqScript.setAttribute("src", "/jquery/jquery.min.js");
-document.head.appendChild(jqScript);
+loadCssStyle("/hux"+huxVersion+"/src/main/hux/fonts/vi-font/css/vi-font.css");
+loadCssStyle("/dejavu/org/vois/base/fonts/dejavu-font/css/dejavu-font.css");
+loadLessStyle("/hux"+huxVersion+"/src/main/hux/themes/vois/less/hux-js-theme.less")
 
-// add requirejs
-let rqScript = document.createElement("script");
-rqScript.setAttribute("src", "/hux"+huxVersion+"/lib/require/require-2.1.17.js");
-document.head.appendChild(rqScript);
 
-// add script.js
-let script = document.createElement("script");
-script.setAttribute("src", "./script.js");
-document.head.appendChild(script);
+loadScript(
+  "/hux"+huxVersion+"/lib/require/require-2.1.17.js", 
+  false,
+  function(){
+    loadScript("./script.js", true); // add user script
+  }
+);// add requirejs
+
--- scenarios/HUX.ui.Button/script.js
+++ scenarios/HUX.ui.Button/script.js
@@ -1,13 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  require(["hux/ui/basic/Button"], function (Button) {
-    new Button({
-      text: "{{var 'pi'}}",
-      renderTo: "body",
-      //disabled: true,
-      scale: "large",
-      default: true,
-      icon :           'vi-meldewesen'
-    })
-  });
-  
+require(["hux/ui/basic/Button"], function (Button) {
+  new Button({
+    text: "{{var 'pi'}}",
+    renderTo: "body",
+    //disabled: true,
+    scale: "large",
+    default: true,
+    icon :           'vi-meldewesen'
+  })
 });
